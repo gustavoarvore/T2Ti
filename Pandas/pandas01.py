079b22b7ae6a1afe05f3401723b4c5c198ddc00f
gustavo <gustavo.machado1p@gmail.com>
2025-09-30 09:40:13 -0300

    Código consertado na parte de DataFrame

--- Pandas/pandas01.py
+++ Pandas/pandas01.py
@@ -7,6 +7,7 @@
 
 import pandas as pd
 
+# Series
 dados = [10,20,30,40,50,60]
 serie = pd.Series(dados, index=['a','b','c','d', 'e','f'])
 
@@ -17,20 +18,20 @@ print(serie + 10)
 
 #DataFrame
 dados = {
-    'nome': ['Alice', 'Gustavo', 'Sara'],
-    'idade': [22, 27, 24],
-    'bairro': ['Rocinha', 'Vila da Penha', 'Vista Alegra']
+    'Nome': ['Alice', 'Gustavo', 'Sara'],
+    'Idade': [22, 27, 24],
+    'Bairro': ['Rocinha', 'Vila da Penha', 'Vista Alegra']
 }
 df = pd.DataFrame(dados)
 print(df)
 print(df.head(2))
 print(df.tail(2))
 print('=============================')
-print(df['nome'])
+print(df['Nome'])
 print('-----------------------------')
 print(df.loc[0])
 print('=============================')
-df['Loucura'] = [12, 15, 45, 60, 70, 80, 100]
+df['Loucura'] = [12, 15, 45]
 print(df)
 print('-----------------------------')
 df = df.drop('Loucura', axis=1)
